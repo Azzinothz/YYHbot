675e638dd918e36aa60428cc173706d6c528cbc8
Azzinothz <30699803+Azzinothz@users.noreply.github.com>
2018-09-20 02:24:29 +0800

    Initial commit

---
+++ Plugin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from CourseStatus import CourseStatus
+
+
+def onQQMessage(bot, contact, member, content):
+    if contact.name == "文明寝室，文明你我他":
+        if content == "yyh hello":
+            bot.SendTo(contact, "YYHbot is ON")
+        
+        if content == "yyh run CourseStatus":
+            CourseStatus().run()
+            
